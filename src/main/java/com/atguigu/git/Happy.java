be917a28b0710a2e3361ef955098f719beb950e3
darkcatx <goodMorning@aliyun.com>
2021-01-27 22:30:32 +0800

    make confilct

--- src/main/java/com/atguigu/git/Happy.java
+++ src/main/java/com/atguigu/git/Happy.java
@@ -3,7 +3,7 @@ package com.atguigu.git;
 public class Happy {
 	
 	public static void main(String[] args) {
-		System.out.println("happy life");
+		System.out.println("happy life right");
 		System.out.println("can not complain");
 	}
 }
